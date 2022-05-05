91ba032a066ff83e39cc781e87aa63bc0b1d0654
rebahama <wiri@hotmail.se>
2022-05-05 17:14:41 +0000

    add all functions

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$("li").css("color","red")//change all li to red
+$("h2").css("text-decoration","underline")//underlines all h2
+$("#my_footer").text("New content")// changes existing html content
+$("#myElement").append("<p> This is a new tag</p>")// appends  a new tag
+$("#my_footer").append("<span> Hello this is </span>")//appends a new span to the my footer id
